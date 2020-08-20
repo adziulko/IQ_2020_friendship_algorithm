889e2c02bc5d80a10f5717d9d2687a9b4da5cbcd
hdedstrom <hdedstrom@dtc-systems.com>
2020-08-20 17:32:20 -0600

    added steps to hannah's game

--- hannah-edstrom.py
+++ hannah-edstrom.py
@@ -11,11 +11,22 @@ def play_game():
 	# if the user selects 1, they want to play! Ask them questions and wait for their answers.
 	while user_entry == 1:
 		## STEP 1 HERE
-		
-		## STEP 2&3 HERE
-	
+		total_points = 0
+                answer_one = int(input('Does your mama dance?\n1. Yes\n2. No\nYour Answer:'))
+                if answer_one == 1: total_points += 5
+                else: total_points += 0
+                ## STEP 2&3 HERE
+                answer_two = int(input('Does your daddy rock n roll?\n1. Yes\n2. No\nYour Answer:'))
+                if answer_two == 1: total_points += 5
+                else: total_points += 0
+                answer_three = int(input('Do you kick off your Sunday shoes?\n1. Yes\n2. No\nYour Answer:'))
+                if answer_three == 1: total_points +=5
+                else: total_points += 0
+
 		## STEP 4 HERE
-	
+                answer_four =  int(input('Are you in the danger zone?\n1. Yes\n 2. No\nYour Answer:'))
+                if answer_four == 1: total_points += 5
+                else: total_points += 0
 		
 		user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
 		user_entry = int(user_entry)
