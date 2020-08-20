804225938c81e005a9c3680e5e2fb1bb8cbc69ee
eli-cipriano <elia.cipriano@colorado.edu>
2020-08-20 17:49:03 -0600

    finished the algorithm!

--- elia_cipriano.py
+++ elia_cipriano.py
@@ -10,7 +10,7 @@ def play_game():
 
 	# ask the user to make their own input (1 to play or 2 to exit):
 	while user_entry != 'y' and i < 6:
-		user_entry = str(input('Do you want to play? (y/n): '))
+		user_entry = str(input('Do you want to play? (y/n): ').lower())
 		if user_entry != 'y':
 			print('you have to play...')
 			i += 1
@@ -18,41 +18,64 @@ def play_game():
 	while user_entry == 'y':
 		## STEP 1 HERE
 		points = 0
-		step1 = str(input('\nDo you play an instrument? (y/n): '))
+		step1 = str(input('\nDo you play an instrument? (y/n): ').lower())
 		if step1 == 'y':
 			points += 50
-		## STEP 2&3 HERE
-
-		step2 = str(input("\nWhat's your favorite music genre?\nfunk\njazz\nrock\nrap\ncountry\n\n: ").lower())
+			print('points + 50')
+		else:
+			print('points + 0')
+		## STEP 2 HERE
+		step2 = str(input("\nWhat's your favorite music genre?\nfunk\njazz\nrock\nrap\ncountry\n\nans: ").lower())
 		if step2 == 'funk':
 			points += 1000
+			print('points + 1000')
 		elif step2 == 'jazz':
 			points += 500
+			print('points + 500')
 		elif step2 == 'rock':
 			points += 100
+			print('points + 100')
 		elif step2 == 'rap':
 			points += 50
+			print('points + 50')
 		elif step2 == 'country':
 			points += 25
+			print('points + 25')
+		else:
+			print('points + 0')
 
-		step3 = str(input("\nDo you love the environment? (y/n): "))
+		## STEP 3 HERE
+		step3 = str(input("\nDo you love the environment? (y/n): ").lower())
 		if step3 == 'y':
 			points += 50
+			print('points + 50')
 		elif step3 == 'n':
 			points -= 1000
+			print('points -1000')
+		else:
+			print('points + 0')
 
 		## STEP 4 HERE
-		step4 = str(input("\nDo you like butter chicken? (y/n): "))
+		step4 = str(input("\nDo you like butter chicken? (y/n): ").lower())
 		if step4 == 'y':
 			points += 100
+			print('points + 100')
 		elif step4 == 'n':
 			points -= 100
-		print('You earned ')
-		user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
-		user_entry = int(user_entry)
+			print('points - 100')
+		else:
+			print('points + 0')
 
+		## STEP 5 HERE (specify points)
+		print('You earned {} points!'.format(points))
+		if points > 1000:
+			print('WE ARE BFFS!!!!')
+		elif points > 0:
+			print('We are friends :)')
+		elif points <= 0:
+			print("Uh oh.. we are not friends...")
 
-
+		user_entry = str(input('Play again? (y/n): ').lower())
 
 ## Function call to play friendship algorithm game
 play_game()
