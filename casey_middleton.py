8ce2eaac9525d44b982b4ca324a6a9aab810748c
caseymid48 <caseymiddleton67@gmail.com>
2020-08-20 17:32:31 -0600

    added all questions and points for q1

--- casey_middleton.py
+++ casey_middleton.py
@@ -13,13 +13,17 @@ def play_game():
 		## STEP 1 HERE
                 total_points = 0
                 answer_one = int(input('Do you like dogs?\n1. Yes\n2. No\nYour answer:'))
-		## STEP 2&3 HERE
+                answer_two = int(input('Do you like to cook?\n1. Yes\n2. No\nYour anser:'))
+                answer_three = int(input('DO you like rock climbing?\n1. Yes\n2. No\nYour answer:'))
+                answer_four = int(input('Do you like to dance?\n1. Yes\n2. No\nYour answer:'))
+                answer_five = int(input('Which is the best Disney princess?\n1. Mulan\n2. Belle\n3.Elsa\n4.Snow White\n5. Rapunzel\nYour answer:'))
+                ## STEP 2&3 HERE
                 if answer_one == 1:
                     total_points += 5
                 else:
                     total_points -= 5
 		## STEP 4 HERE
-	
+                	
 		
 		user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
 		user_entry = int(user_entry)
