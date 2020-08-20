40d6d67aebd29c729337d8c1bb5f39933cff5299
ClairHuffine <clair.huffine@colorado.edu>
2020-08-20 17:31:21 -0600

    added my python file

--- Clair_Huffine.py
+++ Clair_Huffine.py
@@ -14,23 +14,30 @@ def play_game():
 		total_points = 0 
                 answer_one = int(input('Do you want to be friends? \n1. Yes \n2. No\n\nYour Selection: '))
 		## STEP 2&3 HERE
-                elif answer_one == 1 :
+                if answer_one == 1 :
                         input('Sweet! But first, a few important questions...')
-                if answer_one == 2 :
+                elif answer_one == 2 :
                         input("Well...maybe later!")
                         break
                 ## Q2
-                answer_two ==  int(input("Do you absolutely love plants??? \n1. Heck yeah! \n2. Okay maybe not /that/ much...\n\nYour Selection:"))
-                if answer_two == 1 : total_points =+ 5
+                answer_two == int(input("Do you absolutely love plants??? \n1. Heck yeah! \n2. Okay maybe not /that/ much...\n\nYour Selection:"))
+                if answer_two == 1 : total_points += 5
                     answer_two_one = int(input("Do you want more plants? \n1. Never too many \n2. I already have so many to take care of! \n\nYour Selection:"))
-                    if answer_two_one == 1 : total_points =+ 5
-                    elif answer_two_one == 2 : total_points =+ 0
+                    if answer_two_one == 1 : total_points += 5
+                    elif answer_two_one == 2 : total_points += 0
                     else: total_points += 0
                 elif answer_two == 2 : total_points =+ 0 
                 else: total_points += 0
+                
+                ## Q3
+                answer_three == int(input("What is your favorite outdoor activity? \n1. Hiking \n2. Rockclimbing \n3. Running \n\nYour Selection:"))
+                if answer_three == 1 : total_points += 5
+                elif answer_three == 2 : total_points += 0
+                elif answer_three == 3 : total_points -= 5
+                else: total_points += 0
 
-		## STEP 4 HERE
-	
+                ## STEP 4 HERE
+	        if total_points 
 		
 		user_entry = input('Select Option!\n1. Play Game\n2. Exit Game\n\nYour Selection: ')
 		user_entry = int(user_entry)
